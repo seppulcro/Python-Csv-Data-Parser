7185f2ab8e2754966420be2fc593f5aadd5b7863
msacristao <msacristao@gmail.com>
2013-10-21 17:43:43 +0100

    Testing tables and connections

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 # -*- utf-8 -*-
-import xrld
+import xlrd
 import sqlite3
 
 dbconnection = sqlite3.connect("database.db")
-cursor = connect.cursor()
-
-openfile = 
+db = dbconnection.cursor()
+db.execute("DROP TABLE IF EXISTS schools")
+db.execute("CREATE TABLE schools (nome text, district text, courses number)")
+db.execute("DROP TABLE IF EXISTS courses")
+openfile = xlrd.open_workbook("cna131fresultados.xls")
+openfile.sheet_names()
+listindex = openfile.sheet_by_index(0)
