83c02ab03dde706e792fcf921b91e8fa2614eed7
msacristao <msacristao@gmail.com>
2013-10-21 22:26:30 +0100

    String and row tests

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ db.execute("CREATE TABLE schools (nome text, district text, courses number)")
 db.execute("DROP TABLE IF EXISTS courses")
 
 #FILE OPS
-openfile = xlrd.open_workbook("cna131fresultados.xls")
+openfile = xlrd.open_workbook("cna131fresultados.xls", encoding_override="utf-8")
 openfile.sheet_names()
 sheet = openfile.sheet_by_index(0)
 
 for n, s in enumerate(openfile.sheets()):
 	print "#XLRD#\nFile:%s Sheet:%s Columns:%d Rows:%d" % (s.name, n, s.ncols, s.nrows)
 
-print "Isto é um teste"
+print "Teste: Açores, Heroísmo, Nutrição"
 print sheet.row_values(4)
