2ed2f169dbcc79fd34a105deee52f89199c6b3ef
dwjorgeb <dwjorgeb@gmail.com>
2013-10-29 01:24:45 +0000

    Part 1 final - using sqlite3

--- src/main.py
+++ src/main.py
@@ -1,19 +1,35 @@
 # -*- encoding: utf-8 -*-
 import xlrd
-from sqlalchemy import *
-
-db = create_engine('sqlite://database.db')
-metadata = BoundMetaData(db)
-
-
+import sqlite3
 
 #FILE OPS
 openfile = xlrd.open_workbook("cna131fresultados.xls", encoding_override="utf-8")
-openfile.sheet_names()
 sheet = openfile.sheet_by_index(0)
 
-for n, s in enumerate(openfile.sheets()):
-	print "#XLRD#\nFile:%s Sheet:%s Columns:%d Rows:%d" % (s.name, n, s.ncols, s.nrows)
+nrows = sheet.nrows
+ncols = sheet.ncols
 
-print "Teste: Açores, Heroísmo, Nutrição"
-print sheet.row_values(4)
+conn = sqlite3.connect('trabalho.sqlite3')
+cursor = conn.cursor()
+
+cursor.execute("DROP TABLE IF EXISTS inscritos")
+
+cursor.execute('''CREATE TABLE inscritos( cod_inst number, cod_curso number, nome_inst text(100), nome_curso text(100), grau text(2), vagas_inicial number, colocados number, last_grade number, vagas_final number)''')
+
+
+for i in range(3, nrows - 2):
+	cursor.execute('INSERT INTO inscritos VALUES (?,?,?,?,?,?,?,?,?)', sheet.row_values(i))
+
+#everything = [];
+#for x in range(3, nrows - 2):
+#	row = []
+#	for y in range(ncols):
+#		row.append(sheet.cell_value(x,y))
+#		pass
+#	everything.append(row)
+#	pass
+
+#cursor.executemany('INSERT INTO inscritos VALUES (?,?,?,?,?,?,?,?,?)', everything)
+
+conn.commit()
+conn.close()
