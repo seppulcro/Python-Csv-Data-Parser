2ec64246ed1c108e091bc177afe02591f29e81f9
dwjorgeb <dwjorgeb@gmail.com>
2013-10-29 01:42:02 +0000

    Final (1st Part) main.py file

--- src/main.py
+++ src/main.py
@@ -2,34 +2,19 @@
 import xlrd
 import sqlite3
 
-#FILE OPS
+#FILE OPERATIONS
 openfile = xlrd.open_workbook("cna131fresultados.xls", encoding_override="utf-8")
 sheet = openfile.sheet_by_index(0)
 
-nrows = sheet.nrows
-ncols = sheet.ncols
-
+#DATABASE OPERATIONS
 conn = sqlite3.connect('database.db')
 cursor = conn.cursor()
-
 cursor.execute("DROP TABLE IF EXISTS inscritos")
-
 cursor.execute('''CREATE TABLE inscritos( cod_inst number, cod_curso number, nome_inst text(100), nome_curso text(100), grau text(2), vagas_inicial number, colocados number, last_grade number, vagas_final number)''')
 
-
-for i in range(3, nrows - 2):
+#INSERT TO DATABASE.DB ALL ROW VALUES PER ROW STARTING FROM NO.3 AND ENDING IN THE N-2 ROW
+for i in range(3, sheet.nrows - 2):
 	cursor.execute('INSERT INTO inscritos VALUES (?,?,?,?,?,?,?,?,?)', sheet.row_values(i))
-
-#everything = [];
-#for x in range(3, nrows - 2):
-#	row = []
-#	for y in range(ncols):
-#		row.append(sheet.cell_value(x,y))
-#		pass
-#	everything.append(row)
-#	pass
-
-#cursor.executemany('INSERT INTO inscritos VALUES (?,?,?,?,?,?,?,?,?)', everything)
 
 conn.commit()
 conn.close()
