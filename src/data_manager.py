6c87deb1fd6cbd356e13c5a7fceff3009768fb1b
msacristao <msacristao@gmail.com>
2013-12-02 18:35:28 +0000

    data_manager.py - Final version with District handling

--- src/data_manager.py
+++ src/data_manager.py
@@ -27,7 +27,6 @@ SHEET_NUMBER 	= 0
 try:
 	OPEN_FILE = xlrd.open_workbook(FILE_NAME, encoding_override="utf-8")
 	SHEET = OPEN_FILE.sheet_by_index(SHEET_NUMBER)
-	print SHEET.cell(5,1)
 	"""
 	'Row offsets' are used to know from what interval we should start
 	extracting data from, this way we can exclude any rows that do not contain
@@ -43,6 +42,7 @@ except:
 
 # SQAlchemy defining declarative base class, connection and debugging.
 try:
+	# populate_database() needs to be ran in order to Create Database.
 	BASE = declarative_base()
 	DATABASE = create_engine('sqlite:///database.db', echo=True)
 	DB_SESSION = scoped_session(sessionmaker())
@@ -62,8 +62,8 @@ class results(BASE):
 		__tablename__ = "Results"
 		id 					= Column(Integer, primary_key=True)
 		InstitutionCode 	= Column(Integer(4))
-		CourseCode 			= Column(Integer(4))
-		InstitutionName 		= Column(String(100))
+		CourseCode 			= Column(String(4))
+		InstitutionName 	= Column(String(100))
 		CourseName 			= Column(String(100))
 		Degree 				= Column(String(2))
 		InitialOpenings		= Column(Integer(2))
@@ -90,7 +90,9 @@ def populate_database():
 	except:
 		print "Error: Could not create the Database!"
 
-	DATABASE.execute(results.__table__.insert(),
+	# Insert values into Table
+	try:
+		DATABASE.execute(results.__table__.insert(),
 		[{
 		"InstitutionCode": SHEET.cell(i,0).value,
 		"CourseCode": SHEET.cell(i,1).value,
@@ -103,41 +105,54 @@ def populate_database():
 		"RemainingOpenings": SHEET.cell(i,8).value,
 		"District": check_district(SHEET.cell(i,2).value)
 		} for i in range(ROW_INTERVAL_START, ROW_INTERVAL_END)])
-	# try:
-	# 	DATABASE.execute(results.__table__.insert(),
-	# 	[{
-	# 	"InstitutionCode": SHEET.cell(i,0).value,
-	# 	"CourseCode": SHEET.cell(i,1).value,
-	# 	"InstitutionName": SHEET.cell(i,2).value,
-	# 	"CourseName": SHEET.cell(i,3).value,
-	# 	"Degree": SHEET.cell(i,4).value,
-	# 	"InitialOpenings": SHEET.cell(i,5).value,
-	# 	"Placed": SHEET.cell(i,6).value,
-	# 	"LastApplicantGrade": SHEET.cell(i,7).value,
-	# 	"RemainingOpenings": SHEET.cell(i,8).value,
-	# 	"District": check_district(SHEET.cell(i,2).value)
-	# 	} for i in range(ROW_INTERVAL_START, ROW_INTERVAL_END)])
-	# 	print "Successfully inserted values into table!"
-	# except:
-	# 	print "Error: Could not populate database!"
+		print "Successfully inserted values into table!"
+	except:
+		print "Error: Could not populate database!"
 
 def check_district(institution_name):
-	# List of Districts that exist in Portugal
+	# List of districts that exist in Portugal
 	available_districts = ['Lisboa','Porto',u'Setúbal','Braga','Aveiro',
 	'Leiria',u'Santarém','Faro','Coimbra','Viseu','Madeira',u'Açores',
-	'Viana do Castelo','Vila Real','Castelo Branco', u'Évora', 'Guarda', 'Beja'
-	u'Bragança', 'Portalegre']
-	# List of InstituteNames that don't have its district in it.
-	unset_districts = ['Algarve','Minho',u'Trás-os-Montes e Alto Douro']
-	for i in range(0,len(available_districts)-1):
+	'Viana do Castelo','Vila Real','Castelo Branco', u'Évora','Guarda','Beja',
+	u'Bragança','Portalegre']
+
+	'''
+	Checks the Institution name for any available District, if it finds one it 
+	return the district that was found in the name and set it as the 
+	Institution's district
+	'''
+	for i in range(0,len(available_districts)):
 		if(available_districts[i] in institution_name):
 			return available_districts[i]
-		else:
-			for n in range(0, len(unset_districts)-1):
-				if(unset_districts[n] in institution_name):
-					if(n==1): # Algarve is found
-						return available_districts[8] # Return Faro as District
-					elif(n==2): # Minho is found
-						return available_districts[3] # Return Braga as District
-#Execute the whole code.
-populate_database()
+
+	# List of regions to be associated with a district that exists in Portugal
+	unset_districts = ['Algarve','Beira Interior','Minho',
+	u'Trás-os-Montes e Alto Douro',u'Cávado e do Ave','Tomar',
+	u'Náutica Infante D. Henrique','Estoril']
+
+	'''
+	After checking the Institution's name and no available district is found 
+	within, we should convert the Region that we find in the name to an 
+	available district
+	'''
+	for i in range(0, len(unset_districts)):
+		if(unset_districts[i] in institution_name):
+			if(i==0): 	# Region Algarve found
+				return available_districts[7] 	# Return Faro
+			if(i==1): 	# Region Beira Interior(Covilhã) found
+				return available_districts[14]	 # Return Castelo Branco
+			if(i==2): 	# Region Minho found
+				return available_districts[3] 	# Return Braga
+			if(i==3):	# Region Trás-os-Montes e Alto Douro found
+				return available_districts[13]	# Return Vila Real
+			if(i==4): 	# Region Cávado e do Ave found
+				return available_districts[3] 	# Return Braga
+			if(i==5): 	# Region Tomar found
+				return available_districts[6]	# Return Santarém
+			if(i==6): 	# Region Paço de Arcos -> Oeiras
+				return available_districts[0]	# Return Lisboa
+			if(i==7):	# Region Estoril -> Cascais
+				return available_districts[0]	# Return Lisboa
+	return "Unknown District"
+
+# Execute code if needed by using populate_database()
