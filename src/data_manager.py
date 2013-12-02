78cb6d84f3c14470679c22b0116cdb0abb627f6a
msacristao <msacristao@gmail.com>
2013-12-02 05:20:53 +0000

    Updated data_manager.py

--- src/data_manager.py
+++ src/data_manager.py
@@ -18,13 +18,15 @@ import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Float, create_engine
 
-# Global variable with the file's name to open using XLRD
-FILE_NAME = "cna131fresultados.xls"
+# File's name to open using XLRD
+FILE_NAME 		= "cna131fresultados.xls"
+# Sheet number that contains the information
+SHEET_NUMBER 	= 0
 
 # Open FILE_NAME forcing utf-8 encoding and set current sheet as 0
 try:
 	OPEN_FILE = xlrd.open_workbook(FILE_NAME, encoding_override="utf-8")
-	SHEET = OPEN_FILE.sheet_by_index(0)
+	SHEET = OPEN_FILE.sheet_by_index(SHEET_NUMBER)
 	"""
 	'Row offsets' are used to know from what interval we should start
 	extracting data from, this way we can exclude any rows that do not contain
@@ -43,12 +45,12 @@ try:
 	BASE = declarative_base()
 	DATABASE = create_engine('sqlite:///:memory:', echo=True)
 	print "Running SQLAchemy version... " + sqlalchemy.__version__
-	print "Successfully created Database: " + str(DATABASE) + "!"
+	print "Successfully allocated Database: " + str(DATABASE) + "!"
 except:
 	print "Failed to create the SQLAlchemy database!"
 
 
-# Class to which we the SQLAlchemy Table we are going to use.
+# Class to which we the SQLAlchemy table we are going to use.
 class create_table(BASE):
 	"""
 	Creates table to be filled with information from the .XLS file
@@ -65,13 +67,26 @@ class create_table(BASE):
 		Placed				= Column(Integer(3))
 		LastApplicantGrade	= Column(Float(4))
 		RemainingOpenings	= Column(Integer(4))
-		print "Successfully created Table: '" + __tablename__ + "'!"
+		print "Successfully setup table: '" + __tablename__ + "'!"
 	except:
-		print "Failed to create SQAlchemy database's table!"
+		print "Failed to setup SQAlchemy database's table!"
 
-# Function which populates the generated Database's Table
+# Function which populates the generated Database's table
 def populate_database():
+	# Drop the existing database
 	try:
-		BASE.metadata.drop_all(DATABASE)
+		BASE.metadata.drop_all(DATABASE) # Delete table if it exists
 	except:
-		print "Something went wrong."
+		print "Warning: Could not delete the Database!"
+
+	# Create the database
+	try:
+		BASE.metadata.create_all(DATABASE) # Create the Database
+	except:
+		print "Error: Could not create the Database!"
+
+	# Populate the Database's table
+	try:
+		DATABASE.execute()
+	except:
+		print "Error: Could not populate the Database's table."
